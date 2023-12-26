509dc0b24ff10ea59a5af3ebe40c48aea66c9b1a
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2023-12-25 21:00:24 -0800

    update API call

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(response => response.json())
+  //   .then(users => {console.log(users)})
+
+  // fetch('https://swapi.py4e.com/api/people/2/')
+  //   .then(response => response.json())
+  //   .then(users => {console.log(users)})
+
+  // fetch('http://numbersapi.com/42/math')
+  //   .then(response => response.json())
+  //   .then(users => {console.log(users)})
+
+  // fetch('https://api.chucknorris.io/jokes/random')
+  //   .then(response => response.json())
+  //   .then(users => {console.log(users)})
+
+  // fetch('https://pokeapi.co/api/v2/pokemon-species/aegislash')
+  //   .then(response => response.json())
+  //   .then(users => {console.log(users)})
+
+  // fetch('http://api.open-notify.org/iss-now.json')
+  //   .then(response => response.json())
+  //   .then(users => {console.log(users)})
+
+  fetch('https://pokeapi.co/api/v2/pokemon-species/aegislash')
+    .then(response => response.json())
+    .then(users => {console.log(users)})
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +46,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
